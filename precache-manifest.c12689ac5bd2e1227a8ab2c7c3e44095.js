c458521f4c436f0fda3118e8ffd2e3837127f531
Gabriel Vanderlei de Oliveira <gvo@ug3c38.windows.cin.ufpe.br>
2019-08-02 13:05:34 -0300

    a

---
+++ precache-manifest.c12689ac5bd2e1227a8ab2c7c3e44095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a77c365a638a64b06926c020b1b29c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48d54e03e791f885eca1",
+    "url": "/static/css/2.074fc065.chunk.css"
+  },
+  {
+    "revision": "e60bcb41467848b4c0f6",
+    "url": "/static/css/main.ff569719.chunk.css"
+  },
+  {
+    "revision": "48d54e03e791f885eca1",
+    "url": "/static/js/2.e4cd6d8a.chunk.js"
+  },
+  {
+    "revision": "e60bcb41467848b4c0f6",
+    "url": "/static/js/main.df8a5ca7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
